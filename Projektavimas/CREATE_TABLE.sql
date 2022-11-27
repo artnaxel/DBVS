f1ed672671c9ca9e28714ce569e8cd324c0ff106
Aleksandra Kondratjeva <89353862+artnaxel@users.noreply.github.com>
2022-11-28 00:20:10 +0200

    Update CREATE_TABLE.sql

--- Projektavimas/CREATE_TABLE.sql
+++ Projektavimas/CREATE_TABLE.sql
@@ -1,31 +1,36 @@
 CREATE TABLE alko8100.gydytojas (
 
-	AK          CHAR(11)        NOT NULL PRIMARY KEY CHECK (AK > 0),
+	AK          CHAR(11)        NOT NULL CONSTRAINT AsmensKodas CHECK (AK > 0),
 	Vardas      VARCHAR(20)     NOT NULL,
 	Pavarde	    VARCHAR(20)	    NOT NULL,
 	Pareigos    VARCHAR(20)     NOT NULL,
 	Atlyginimas DECIMAL(7,2)    NOT NULL CONSTRAINT Alga CHECK (Atlyginimas >= 1000 AND Atlyginimas <= 10000),
 
+    PRIMARY KEY (AK)
+
 );
 
 CREATE TABLE alko8100.apsilankymas (
 
-    Nr          SERIAL      NOT NULL PRIMARY KEY,
+    Nr          SERIAL      NOT NULL,
     Pacientas   CHAR(11)    NOT NULL,
     Gydytojas   CHAR(11)    NOT NULL,
     Laikas      TIME        NOT NULL,
     Data        DATE        NOT NULL,
 
+    PRIMARY KEY (Nr),
     FOREIGN KEY (Gydytojas) REFERENCES alko8100.gydytojas(AK) ON DELETE RESTRICT ON UPDATE RESTRICT,
     FOREIGN KEY (Pacientas) REFERENCES alko8100.pacientas(AK) ON DELETE CASCADE  ON UPDATE RESTRICT
 );
 
 CREATE TABLE alko8100.priezastis (
 
-    Apsilankymas    SERIAL          NOT NULL PRIMARY KEY,
-    Liga            VARCHAR(20)     NOT NULL PRIMARY KEY,
+    Apsilankymas    SERIAL          NOT NULL,
+    Liga            VARCHAR(20)     NOT NULL,
     Pavadinimas     VARCHAR(20)     NOT NULL,
 
+    PRIMARY KEY (Apsilankymas),
+    PRIMARY KEY (Liga),
     FOREIGN KEY (Apsilankymas)      REFERENCES alko8100.apsilankymas(Nr) ON DELETE CASCADE  ON UPDATE RESTRICT,
     FOREIGN KEY (Liga)              REFERENCES alko8100.liga(Ligos_nr)   ON DELETE CASCADE  ON UPDATE RESTRICT
 
@@ -33,28 +38,34 @@ CREATE TABLE alko8100.priezastis (
 
 CREATE TABLE alko8100.pacientas (
 
-    AK              CHAR(11)        NOT NULL PRIMARY KEY CHECK (AK > 0),
+    AK              CHAR(11)        NOT NULL CONSTRAINT AsmensKodas CHECK (AK > 0),
     Vardas          VARCHAR(2)      NOT NULL,
 	Pavarde	        VARCHAR(20)	    NOT NULL,
     Gimimo_data     DATE            NOT NULL,
-    Lytis           CHAR(7)         NOT NULL CHECK (Lytis IN ('Moteris', 'Vyras')),
+    Lytis           CHAR(7)         NOT NULL CONSTRAINT NurodytaLytis CHECK (Lytis IN ('Moteris', 'Vyras')),
     Telefono_nr	    CHAR(15)	    NOT NULL,
+
+    PRIMARY KEY (AK)
 );
 
 CREATE TABLE alko8100.serga (
 
-    Pacientas           CHAR(11)        NOT NULL PRIMARY KEY,
-    Liga                VARCHAR(20)     NOT NULL PRIMARY KEY,
+    Pacientas           CHAR(11)        NOT NULL,
+    Liga                VARCHAR(20)     NOT NULL,
     Diagnozavimo_data   DATE            NOT NULL,
 
+    PRIMARY KEY (Pacientas),
+    PRIMARY KEY (Liga),
     FOREIGN KEY (Pacientas)             REFERENCES alko8100.pacientas(AK)    ON DELETE CASCADE      ON UPDATE RESTRICT,
-    FOREIGN KEY (Liga)                  REFERENCES alko8100.liga(Ligos_nr)   ON DELETE RESTRICT      ON UPDATE RESTRICT
+    FOREIGN KEY (Liga)                  REFERENCES alko8100.liga(Ligos_nr)   ON DELETE RESTRICT     ON UPDATE RESTRICT
 
 );
 
 CREATE TABLE alko8100.liga (
 
-    Ligos_nr        CHAR(15)         NOT NULL PRIMARY KEY,
+    Ligos_nr        CHAR(15)         NOT NULL,
     Pavadinimas     VARCHAR(15)      NOT NULL,
+
+    PRIMARY KEY (Ligos_nr)
 );
     
